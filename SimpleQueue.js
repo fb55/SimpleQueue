1bff9f9ad28aacb5bbb4fe19554c3e2261d3ec1d
Felix Böhm <me@feedic.com>
2012-02-10 15:59:42 +0100

    Added some utility methods
    • abort stops as much as it can
    • pause pauses the queue
    • resume is the contrary of pause

--- SimpleQueue.js
+++ SimpleQueue.js
@@ -8,6 +8,7 @@ var SimpleQueue = function(worker, callback, done, concurrent){
 	this._working = 0;
 	this._lastStarted = 0;
 	this._finished = 0;
+	this._paused = false;
 };
 
 SimpleQueue.prototype.push = function(props){
@@ -15,9 +16,37 @@ SimpleQueue.prototype.push = function(props){
 	this._scan();
 };
 
+SimpleQueue.prototype.abort = function(){
+	//clears the queue (can't stop running processes)
+	this._queue.splice(0);
+	this._paused = true; //cb won't be called any more
+};
+
+SimpleQueue.prototype.pause = function(){
+	this._paused = true;
+};
+
+SimpleQueue.prototype.resume = function(){
+	this._paused = false;
+	this._scan();
+	this._checkStack();
+}
+
+SimpleQueue.prototype._checkStack = function(){
+	while(this._stack[this._finished]){
+	    this._callback.apply(this, this._stack[this._finished]);
+	    this._stack[this._finished] = null;
+	    this._finished += 1;
+	}
+	if(this._working === 0 && this._queue.length === 0 && this._done){
+	    this._done();
+	}
+};
+
 SimpleQueue.prototype._scan = function(){
 	if( this._working === this._concurrent || 
-		this._queue.length === 0) return;
+		this._queue.length === 0 ||
+		this._paused) return;
 
 	var element = this._queue.shift(),
 		index = this._lastStarted++,
@@ -27,15 +56,10 @@ SimpleQueue.prototype._scan = function(){
 
 	this._worker(element, function(err, result){
 		that._working--;
-		if(index === that._finished){
+		if(!that._paused && index === that._finished){
 			that._callback(err, result, element);
-			while(that._stack[++that._finished]){
-				that._callback.apply(that, that._stack[that._finished]);
-				that._stack[that._finished] = null;
-			}
-			if(that._working === 0 && that._queue.length === 0 && that._done){
-				that._done();
-			}
+			that._finished += 1;
+			that._checkStack();
 		}
 		else that._stack[index] = [err, result, element];
 		
